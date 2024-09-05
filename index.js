70426e3cf25b101a800914ec94f6dcaf45363297
DedSec2050 <marsalsoren2050@gmail.com>
2024-09-05 23:34:15 +0530

    correct cd

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post("/posts", async (c) => {
 });
 
 // GET Requests
-app.get("/", (c) => c.text("Hello World! Rev3"));
+app.get("/", (c) => c.text("Hello World! Rev4"));
 
 app.get("/posts", async (c) => {
   try {
